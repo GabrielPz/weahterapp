d8ecd1b02305d7e8375b9fc17b29099a27e5772c
Gabriel <gabrielhenriquedps@gmail.com>
2024-03-10 23:41:12 -0300

    style: fix navigation boxshadow

--- src/App.vue
+++ src/App.vue
@@ -138,5 +138,19 @@ export default {
   .container {
     padding: 0 20px;
   }
+
+  .day {
+    transition: 500ms ease all;
+    background-color: rgb(59, 150, 249);
+    box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
+      0 2px 4px -1px rgba(0, 0, 0, 0.06);
+  }
+
+  .night {
+    transition: 500ms ease all;
+    background-color: rgb(20, 42, 95);
+    box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
+      0 2px 4px -1px rgba(0, 0, 0, 0.06);
+  }
 }
 </style>
